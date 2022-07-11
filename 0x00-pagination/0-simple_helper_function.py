47fa6869fd3d76a773c284f5eba5b1fe2a5179db
Joseun <ologanj@gmail.com>
2022-07-11 21:39:31 +0000

    Pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -15,5 +15,4 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     start_index = (page * page_size) - page_size
     end_index = start_index + page_size
-    return (start_index, end_index)
-    
+    return(start_index, end_index)
