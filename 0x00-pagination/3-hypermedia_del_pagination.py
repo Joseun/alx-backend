2de2b1403447ea9e616231056e951a1ca3dd1614
Joseun <ologanj@gmail.com>
2022-07-11 21:40:38 +0000

    Pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -17,7 +17,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     start_index = (page * page_size) - page_size
     end_index = start_index + page_size
-    return (start_index, end_index)
+    return(start_index, end_index)
 
 
 class Server:
